dfdd3b5ee9da5847099f5329a9f3bda88bfafd7f
Arjun Puri <arjunpuri@arjunpuri.me>
2022-01-14 14:56:52 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+import { typeDefs } from './typeDefs';
+import { resolvers } from './resolvers';
+import mongoose from 'mongoose';
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers);
